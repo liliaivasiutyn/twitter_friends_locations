e4d26c7566e75bf127e17c9793018d48127e7b1c
liliaivasiutyn <ivasiutyn@ucu.edu.ua>
2020-02-25 13:28:39 +0000

    changed hidden.py

--- hidden.py
+++ hidden.py
@@ -4,7 +4,7 @@
 # Create new App and get the four strings
 
 def oauth():
-    return {"consumer_key": "sLQGIiDb1zA1iJkHQcNyqy1fB",
-            "consumer_secret": "Rcr7YYrAeMxsQrVvCdO1aBZWJdnhdleiIaK7mfHNsJJZ4pCWVm",
-            "token_key": "1230847676222144514-UIgOvFOZi6CIgBBGXrC2YJA0kQ60EI",
-            "token_secret": "t07b6Gzu3hrWnOd2IcpesUGqjlyZvfQSwBqxckhkAzp52"}
+    return {"consumer_key": "XXXXXXXXXXXXXXXXXX",
+            "consumer_secret": "XXXXXXXXXXXXXXXXXXX",
+            "token_key": "XXXXXXXXXXXXXXXXXXXXXXX",
+            "token_secret": "XXXXXXXXXXXXXXXXXXXXXXXXXX"}
